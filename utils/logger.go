c7aacd7b211890d8f448e857df9bdffe09f3ac2c
aatomu <aatomu21263+git@gmail.com>
2024-02-09 00:27:20 +0900

    Private担っていたのを修正

--- utils/logger.go
+++ utils/logger.go
@@ -9,22 +9,22 @@ import (
 	"time"
 )
 
-type loggerHandler struct {
+type LoggerHandler struct {
 	directory string
 }
 
-func Logger(dir string) (l *loggerHandler, err error) {
+func Logger(dir string) (l *LoggerHandler, err error) {
 	err = os.MkdirAll(dir, 0755)
 	if err != nil {
 		return
 	}
-	l = &loggerHandler{
+	l = &LoggerHandler{
 		directory: dir,
 	}
 	return
 }
 
-func (l *loggerHandler) Info(arg ...any) {
+func (l *LoggerHandler) Info(arg ...any) {
 	pc, file, line, _ := runtime.Caller(1)
 	f := runtime.FuncForPC(pc)
 	text := fmt.Sprintf("[%s] INFO %s(%s:%d) Message:%s", time.Now().Format("2006-01-02T15:04:05.000"), f.Name(), filepath.Base(file), line, fmt.Sprint(arg...))
@@ -32,7 +32,15 @@ func (l *loggerHandler) Info(arg ...any) {
 	io.WriteString(os.Stdout, text)
 }
 
-func (l *loggerHandler) Error(arg ...any) {
+func (l *LoggerHandler) Warn(arg ...any) {
+	pc, file, line, _ := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+	text := fmt.Sprintf("[%s] Warn %s(%s:%d) Message:%s", time.Now().Format("2006-01-02T15:04:05.000"), f.Name(), filepath.Base(file), line, fmt.Sprint(arg...))
+
+	io.WriteString(os.Stdout, text)
+}
+
+func (l *LoggerHandler) Error(arg ...any) {
 	pc, file, line, _ := runtime.Caller(1)
 	f := runtime.FuncForPC(pc)
 	text := fmt.Sprintf("[%s] ERROR %s(%s:%d) Message:%s", time.Now().Format("2006-01-02T15:04:05.000"), f.Name(), filepath.Base(file), line, fmt.Sprint(arg...))
